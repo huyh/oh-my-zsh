ec1601de0d784ff540237bf87e238478d64517d3
Huy Hoang <huythieuhoang@gmail.com>
2012-09-06 12:19:54 +1000

    Removed unused files

--- custom/env.zsh
+++
@@ -1,4 +0,0 @@
-# Customize to your needs...
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
-
-export PATH=/usr/local/bin:$PATH:/opt/local/bin:/usr/local/mysql/bin:/Users/huy/dev/gradle-1.0-milestone-9/bin
